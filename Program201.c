ceb4ffa7d39372065e89d583bffe83948eb25754
Shareen-14 <67071052+Shareen-14@users.noreply.github.com>
2022-04-25 00:56:52 +0530

    Add files via upload

---
+++ Program201.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+	int Data;
+	struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+int main()
+{
+	PNODE First = NULL;
+	NODE obj;
+	//struct node * First = NULL;
+	
+	return 0;
+}
