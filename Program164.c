28365a193ecda83f7ab06722c4f8d2d8ede9612d
Shareen-14 <67071052+Shareen-14@users.noreply.github.com>
2022-04-25 00:56:06 +0530

    Add files via upload

---
+++ Program164.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<io.h>
+#include<fcntl.h>
+
+int main()
+{
+	int fd = 0, iRet = 0;
+	char Fname [30];
+	char Data[7];
+	
+	printf("Enter the file name\n");
+	scanf("%s",Fname);
+	
+	fd = open(Fname,O_RDWR);
+	
+	if(fd == -1)
+	{
+		printf("Unable to open the file\n");
+		return -1; //return to OS
+	}
+	else
+	{
+		printf("File successfully opened with FD : %d\n",fd);
+	}
+	iRet = read(fd,Data,6);
+	
+	write(1,Data,iRet);
+	close(fd);
+	
+	return 0;
+}
